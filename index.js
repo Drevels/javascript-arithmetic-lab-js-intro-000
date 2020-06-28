bb99cbb2cb3a9091f263616f65ed9c41e733e834
danielle revels <ddaniellerrevels@aol.com>
2020-06-28 04:32:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ function increment(n){
   return n += 1;
 }
 function increment(n){
-  return n -= 1;
+  return n ++ 1;
 }
 function makeInt(n){
   return parseInt(n, 10);
 }
 function preserveDecimal(n){
   return parseInt(n);
+}
+function decrement(n){
+  return n -= 1;
 }
